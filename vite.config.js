5fe92bd657dda388fca97ef6ad137a79d3f944c2
DAYALIN1 <00042423@uca.edu.sv>
2024-12-04 10:53:29 -0600

    configuracion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import pugPlugin from 'vite-plugin-pug';
 
 export default defineConfig({
-  plugins: [react(), pugPlugin()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  server: {
+    port: 3000,
+  },
+  base: './',
 });
